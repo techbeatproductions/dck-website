fdcf01109afce843126eaa5cf474f3dee71ce33c
techbeatproductions <ngetichkevin67@gmail.com>
2022-09-14 20:06:55 +0300

    navbar links finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React from 'react';
 import {Link} from 'react-router-dom';
+import { GiClick } from 'react-icons/gi';
+import { AiFillHome } from 'react-icons/ai';
+import { FaChurch, FaDonate, FaBible } from 'react-icons/fa';
+import Logo from '../static/logo.svg'
+
+
+
 
 function Button({text, bg, padding}) {
   return (
@@ -20,17 +27,41 @@ function Navbar() {
   return (
     <div className="fixed left-0 right-0 top-0 h-16 shadow-md border-b-2 border-gray-100 bg-primary2">
       <nav className="flex items-center container mx-auto h-full justify-between">
-        <h1 className="font-semibold uppercase text-lg text-accent">
-          🔄 Deliverance Church Kayole
+        <div className='flex flex-row items-center'>
+          <img src={Logo} alt="" className='h-8'/>
+          <h1 className="font-thin  text-xs text-accent">
+           Deliverance Church Kayole
         </h1>
+        </div>
+        
         <div>
           <ul className="flex items-center space-x-10 text-sm">
-            <li><Link to="/" className="text-accent hover:text-accent">Home</Link></li>
-            <li><Link to="/about" className="text-accent hover:text-accent">About Us</Link></li>
+            <div className='flex flex-row items-center space-x-1'>
+              <AiFillHome className='fill-current text-accent'/>
+
+              <li><Link to="/" className="text-accent hover:text-accent"><p className='font-sans text-xs font-thin '>Home</p></Link></li>
+            </div>
             
-            <li><Link to="/ministries" className="text-accent hover:text-accent">Ministries</Link></li>
-            <li><Link to="/preachings" className="text-accent hover:text-accent">Preachings</Link></li>
-            <li><Link to="/give" className="text-accent hover:text-accent">Give</Link></li>
+            <div className='flex flex-row items-center space-x-1'>
+              <GiClick className='fill-current text-accent'/>
+              <li><Link to="/about" className="text-accent hover:text-accent"><p className='font-sans text-xs font-thin'>About Us</p></Link></li>
+            </div>
+            
+            <div className='flex flex-row  items-center space-x-1'>
+              <FaChurch className='fill-current text-accent' />
+               <li><Link to="/ministries" className="text-accent hover:text-accent"><p className='font-sans text-xs font-thin '>Ministries</p></Link></li>
+            </div>
+
+            <div className='flex flex-row items-center space-x-1'>
+              <FaBible className='fill-current text-accent' />
+              <li><Link to="/preachings" className="text-accent hover:text-accent"><p className='font-sans text-xs font-thin'>Preachings</p></Link></li>
+            </div>        
+
+            <div className='flex flex-row items-center space-x-1'>
+              <FaDonate className='fill-current text-accent' />
+              <li><Link to="/give" className="text-accent hover:text-accent"><p className='font-sans text-xs font-thin'>Give</p></Link></li>
+              </div>
+            
           </ul>
         </div>
        
